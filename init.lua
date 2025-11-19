fefa09159a07d5e87c6da898851f67f5e3291313
bowen <693015917@qq.com>
2025-11-19 13:47:07 +0800

    v0.2

--- init.lua
+++ init.lua
@@ -1,3 +1,18 @@
 require("options")
 require("keymap")
 require("autopairs").setup()
+if vim.g.neovide then
+  vim.o.guifont = "Maple Mono NF:h18"
+  vim.g.neovide_floating_blur_amount_x = 5.0
+  vim.g.neovide_floating_blur_amount_y = 3.0
+  vim.g.neovide_floating_corner_radius = 0.8
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 15
+  vim.g.neovide_opacity = 1
+  vim.g.neovide_normal_opacity = 1
+  vim.g.neovide_cursor_vfx_mode = "wireframe"
+  vim.g.neovide_cursor_vfx_particle_lifetime = 1.2
+  vim.g.neovide_cursor_vfx_particle_speed = 8.0
+  vim.g.neovide_refresh_rate_idle = 5
+  vim.g.neovide_fullscreen = false
+end
--- lua/autopairs.lua
+++ lua/autopairs.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local api = vim.api
+
 local keys = {
   left = "<Left>",
   right = "<Right>",
@@ -48,8 +50,8 @@ local function merge_pairs(base, extra)
 end
 
 local function is_disabled(disable_set)
-  local buf = vim.api.nvim_get_current_buf()
-  local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+  local buf = api.nvim_get_current_buf()
+  local ft = vim.bo.filetype
   return disable_set[ft]
 end
 
@@ -60,10 +62,6 @@ local function char_at(line, index)
   return line:sub(index, index)
 end
 
-local function is_word_char(char)
-  return char and char:match("%w") ~= nil
-end
-
 local function build_close_lookup(pair_map)
   local lookup = {}
   for open, close in pairs(pair_map) do
@@ -72,57 +70,84 @@ local function build_close_lookup(pair_map)
   return lookup
 end
 
-local function wrap_quote(open, prev_char)
-  return true
+local function is_open_char(char, map)
+  return char and map[char]
+end
+
+local function is_close_char(char, lookup)
+  return char and lookup[char]
+end
+
+local function analyze_context(line, col, pair_map, close_lookup)
+  local stack = {}
+  local i = 1
+  while i <= #line do
+    local ch = line:sub(i, i)
+    local close = pair_map[ch]
+    local opener = close_lookup[ch]
+
+    if close and ch ~= '\'' and ch ~= '"' and ch ~= '`' then
+      table.insert(stack, { char = ch, pos = i, close = close })
+    elseif opener and ch ~= '\'' and ch ~= '"' and ch ~= '`' then
+      if stack[#stack] and stack[#stack].char == opener then
+        table.remove(stack)
+      end
+    end
+
+    if (ch == '\'' or ch == '"' or ch == '`') then
+      if stack[#stack] and stack[#stack].char == ch then
+        table.remove(stack)
+      else
+        table.insert(stack, { char = ch, pos = i, close = ch })
+      end
+    end
+
+    if i >= col then
+      break
+    end
+    i = i + 1
+  end
+
+  return stack[#stack]
 end
 
 function M.setup(opts)
   opts = opts or {}
   local config = {}
   config.pairs = merge_pairs(default_opts.pairs, opts.pairs)
+  config.close_lookup = build_close_lookup(config.pairs)
   config.disable_filetypes = to_set(opts.disable_filetypes or default_opts.disable_filetypes)
   config.map_backspace = opts.map_backspace == nil and default_opts.map_backspace or opts.map_backspace
   M.config = config
-  M.close_lookup = build_close_lookup(config.pairs)
+  M.close_lookup = config.close_lookup
 
   local set = vim.keymap.set
   local km_opts = { expr = true, silent = true, noremap = true, desc = "autopairs" }
 
   for open, close in pairs(config.pairs) do
-    if open == close then
-      set("i", open, function()
-        if next(config.disable_filetypes) ~= nil and is_disabled(config.disable_filetypes) then
-          return open
-        end
-        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-        local line = vim.api.nvim_get_current_line()
-        local next_char = char_at(line, col + 1)
-        if next_char == close then
+    set("i", open, function()
+      if next(config.disable_filetypes) ~= nil and is_disabled(config.disable_filetypes) then
+        return open
+      end
+      local cursor = api.nvim_win_get_cursor(0)
+      local row, col = cursor[1], cursor[2]
+      local line = api.nvim_get_current_line()
+      local next_char = char_at(line, col + 1)
+      local context = analyze_context(line, col, config.pairs, config.close_lookup)
+
+      if open == close then
+        if context and context.char == open and next_char == close then
           return keys.right
         end
-        if next_char and next_char:match("%S") and next_char ~= close then
-          return open
-        end
         return open .. close .. keys.left
-      end, km_opts)
-    else
-      set("i", open, function()
-        if next(config.disable_filetypes) ~= nil and is_disabled(config.disable_filetypes) then
-          return open
-        end
-        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-        local line = vim.api.nvim_get_current_line()
-        local prev_char = char_at(line, col)
-        if not wrap_quote(open, prev_char) then
-          return open
-        end
-        local next_char = char_at(line, col + 1)
-        if next_char and next_char:match("%S") and next_char ~= close then
-          return open
-        end
-        return open .. close .. keys.left
-      end, km_opts)
-    end
+      end
+
+      if next_char and next_char:match("%S") and not is_close_char(next_char, config.close_lookup) then
+        return open
+      end
+
+      return open .. close .. keys.left
+    end, km_opts)
   end
 
   for close, open in pairs(M.close_lookup) do
@@ -131,12 +156,20 @@ function M.setup(opts)
         if next(config.disable_filetypes) ~= nil and is_disabled(config.disable_filetypes) then
           return close
         end
-        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-        local line = vim.api.nvim_get_current_line()
+        local cursor = api.nvim_win_get_cursor(0)
+        local row, col = cursor[1], cursor[2]
+        local line = api.nvim_get_current_line()
         local next_char = char_at(line, col + 1)
+        local context = analyze_context(line, col, config.pairs, config.close_lookup)
+
         if next_char == close then
           return keys.right
         end
+
+        if context and context.char == open then
+          return close
+        end
+
         return close
       end, km_opts)
     end
@@ -147,8 +180,9 @@ function M.setup(opts)
       if next(config.disable_filetypes) ~= nil and is_disabled(config.disable_filetypes) then
         return keys.backspace
       end
-      local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-      local line = vim.api.nvim_get_current_line()
+      local cursor = api.nvim_win_get_cursor(0)
+      local row, col = cursor[1], cursor[2]
+      local line = api.nvim_get_current_line()
       local prev_char = char_at(line, col)
       local next_char = char_at(line, col + 1)
       if prev_char and next_char and config.pairs[prev_char] == next_char then
